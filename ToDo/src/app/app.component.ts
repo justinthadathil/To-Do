459337eeef930635e0a2aa57f267da6ffad3e427
Justin Thadathil <justin.quickdesks@gmail.com>
2021-01-27 23:10:57 +0530

    edit and delete

--- ToDo/src/app/app.component.ts
+++ ToDo/src/app/app.component.ts
@@ -40,8 +40,24 @@ export class AppComponent {
     );
   }
 
-  edit(list: string, task:Task): void {
+  edit(list: 'done' | 'too' | 'inProgress', task:Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data:{
+        task,
+        enableDelete: true
+      }
+    });
 
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
+      const dataList = this[list];
+      const taskIndex =dataList.indexOf(task);
+      if(result.delete){
+        dataList.splice(taskIndex, 1);
+      }else{
+        dataList[taskIndex] = task;
+      }
+    })
   }
 
   newTask(): void{
